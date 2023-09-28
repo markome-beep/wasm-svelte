3f0d1fad879ebaf80ee370b042eb55d5f57f8733
markome-beep <guyofsoccer@gmail.com>
2023-09-28 00:15:02 -0400

    stuff

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import wasmPack from "vite-plugin-wasm-pack";
 
-wasmPack("./wasm-test")
+wasmPack("./wasm-test");
 export default defineConfig({
 	plugins: [
 		sveltekit(),
